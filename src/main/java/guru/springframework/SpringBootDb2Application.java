81caaeb941cfc705f1c378aa7d4a86f95db22645
Chad Lou <chadlou@us.ibm.com>
2020-05-11 21:41:36 -0700

    convert to web application

--- src/main/java/guru/springframework/SpringBootDb2Application.java
+++ src/main/java/guru/springframework/SpringBootDb2Application.java
@@ -2,9 +2,10 @@ package guru.springframework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDb2Application {
+public class SpringBootDb2Application extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDb2Application.class, args);
--- src/test/java/guru/springframework/SpringBootDb2ApplicationTests.java
+++ src/test/java/guru/springframework/SpringBootDb2ApplicationTests.java
@@ -4,7 +4,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
+import org.springframework.test.context.web.WebAppConfiguration;
+@WebAppConfiguration
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringBootDb2ApplicationTests {
